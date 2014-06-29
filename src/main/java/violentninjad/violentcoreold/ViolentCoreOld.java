4b54e916a8182419722f3fdff14482dbabe27920
ViolentNinjaD <fiddlybilly@gmail.com>
2014-06-29 09:49:05 +0100

    Setting up LogHelper and more events in the main class

--- src/main/java/violentninjad/violentcoreold/ViolentCoreOld.java
+++ src/main/java/violentninjad/violentcoreold/ViolentCoreOld.java
@@ -1,10 +1,37 @@
 package violentninjad.violentcoreold;
 
 import violentninjad.violentcoreold.reference.ModInfo;
+import violentninjad.violentcoreold.util.LogHelper;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
 
 @Mod(modid=ModInfo.MOD_ID, name=ModInfo.MOD_NAME, version=ModInfo.MOD_VERSION)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class ViolentCoreOld {
+	
+	 @Instance(ModInfo.MOD_ID)
+	 public static ViolentCoreOld instance;
+	 
+	 @EventHandler
+	 public void preInit(FMLPreInitializationEvent event) {
+		 LogHelper.init();
+	 }
+	 
+	 @EventHandler
+	 public void init(FMLInitializationEvent event) {
+		 
+	 }
+	 
+	 @EventHandler
+	 public void postInit(FMLPostInitializationEvent event) {
+		 
+	 }
+
 
 }
--- src/main/java/violentninjad/violentcoreold/util/LogHelper.java
+++ src/main/java/violentninjad/violentcoreold/util/LogHelper.java
@@ -1,5 +1,62 @@
 package violentninjad.violentcoreold.util;
 
+import java.util.logging.*;
+
+import cpw.mods.fml.common.FMLLog;
+import violentninjad.violentcoreold.reference.ModInfo;
+
 public class LogHelper {
+	
+	private static Logger ViolentCoreLogger = Logger.getLogger(ModInfo.MOD_ID);
+	
+	public static void init()
+    {
+        ViolentCoreLogger.setParent(FMLLog.getLogger());
+    }
+
+    public static void log(Level logLevel, Object object)
+    {
+        ViolentCoreLogger.log(logLevel, String.valueOf(object));
+    }
+
+    public static void severe(Object object)
+    {
+        log(Level.SEVERE, object);
+    }
+
+    public static void debug(Object object)
+    {
+        log(Level.INFO, String.format("[DEBUG] %s", String.valueOf(object)));
+    }
+
+    public static void warning(Object object)
+    {
+        log(Level.WARNING, object);
+    }
+
+    public static void info(Object object)
+    {
+        log(Level.INFO, object);
+    }
+
+    public static void config(Object object)
+    {
+        log(Level.CONFIG, object);
+    }
+
+    public static void fine(Object object)
+    {
+        log(Level.FINE, object);
+    }
+
+    public static void finer(Object object)
+    {
+        log(Level.FINER, object);
+    }
+
+    public static void finest(Object object)
+    {
+        log(Level.FINEST, object);
+    }
 
 }
